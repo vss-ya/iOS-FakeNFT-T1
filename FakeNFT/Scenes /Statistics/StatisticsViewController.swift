7a03ce11f81e6b14f6507afa7e4ce201923baf74
Heepsound <heepsound@yandex.ru>
2024-06-18 19:48:09 +0300

    Оптимизирован код в StatisticsViewController

--- FakeNFT/Scenes /Statistics/StatisticsViewController.swift
+++ FakeNFT/Scenes /Statistics/StatisticsViewController.swift
@@ -30,17 +30,9 @@ final class StatisticsViewController: UIViewController {
     
     // MARK: - Lifecycle
     
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        bind()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     func applyViewModel(_ newViewModel: StatisticsCollectionsViewModelProtocol) {
         viewModel = newViewModel
+        bind()
         viewModel?.initialize()
     }
     
