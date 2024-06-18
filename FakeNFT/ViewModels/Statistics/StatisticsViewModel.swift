97740d3b3dcc179570c5e22b1335ae013c3104ce
Heepsound <heepsound@yandex.ru>
2024-06-18 21:09:22 +0300

    Добавлен StatisticsDataStore. Моковые данные о пользователях перенесены в StatisticsDataStore. StatisticsViewModel получает данные из StatisticsDataStore

--- FakeNFT/ViewModels/Statistics/StatisticsViewModel.swift
+++ FakeNFT/ViewModels/Statistics/StatisticsViewModel.swift
@@ -8,44 +8,7 @@
 import Foundation
 
 final class StatisticsViewModel: StatisticsCollectionsViewModelProtocol {
-    let profiles: [StatisticsUser] = [
-        StatisticsUser(
-            name: "Alex",
-            avatar: "https://code.s3.yandex.net/landings-v2-ios-developer/space.PNG",
-            description: "Дизайнер из Казани, люблю цифровое искусство  и бейглы. В моей коллекции уже 100+ NFT,  и еще больше — на моём сайте. Открыт  к коллаборациям.",
-            website: "https://practicum.yandex.ru/ios-developer",
-            nfts: ["", "", ""],
-            rating: "1",
-            id: "1"
-        ),
-        StatisticsUser(
-            name: "Bill",
-            avatar: "https://code.s3.yandex.net/landings-v2-ios-developer/space.PNG",
-            description: "Описание Била",
-            website: "https://yandex.ru",
-            nfts: ["", "", "", "", "", ""],
-            rating: "2",
-            id: "2"
-        ),
-        StatisticsUser(
-            name: "Alla",
-            avatar: "",
-            description: "Описание Аллы",
-            website: "https://figma.com",
-            nfts: ["", "", "", "", ""],
-            rating: "3",
-            id: "3"
-        ),
-        StatisticsUser(
-            name: "Mads",
-            avatar: "https://code.s3.yandex.net/landings-v2-ios-developer/space.PNG",
-            description: "Описание Мэда",
-            website: "https://practicum.yandex.ru/",
-            nfts: ["", "", "", ""],
-            rating: "4",
-            id: "4"
-        )
-    ]
+    let dataStore = StatisticsDataStore.shared
     
     var updateData: Binding<Bool>?
     
@@ -54,11 +17,11 @@ final class StatisticsViewModel: StatisticsCollectionsViewModelProtocol {
     }
     
     func numberOfRowsInSection(_ section: Int) -> Int {
-        return profiles.count
+        return dataStore.users.count
     }
     
     func model(at indexPath: IndexPath) -> Decodable {
-        return profiles[indexPath.row]
+        return dataStore.users[indexPath.row]
     }
     
     func initialize() {
